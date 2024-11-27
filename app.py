a2cfe866a584cd94e2c1dc4f8f446c4b6ca793e0
Lakshmisanajanasb9 <125407142+Lakshmisanajanasb9@users.noreply.github.com>
2024-11-27 22:09:32 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+
+def add(a, b):
+    """Function to add two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Function to subtract two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    print("Simple Python application")
